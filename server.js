c33fe45b16b13e653f90861066c7bad2a9c2b7e6
kswhite15 <58196468+kswhite15@users.noreply.github.com>
2020-09-27 16:14:55 -0400

    lab 4

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ app.route('/api')
   })
   .post((req, res) => {
     console.log('POST request detected');
+    res.send('hello world');
   });
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
