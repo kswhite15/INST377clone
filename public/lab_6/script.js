9660ece32c10c01b588158de4e801f051d712488
kswhite15 <58196468+kswhite15@users.noreply.github.com>
2020-10-24 23:06:49 -0400

    lab6js

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -8,13 +8,17 @@ function range(int) {
   return arr;
 }
 
-function sortFunction(a, b, key) {
+function sortByKey(a, b, key) {
   if (a[key] < b[key]) {
     return -1;
   } if (a[key] > b[key]) {
     return 1;
   }
   return 0;
+}
+
+function getRandomIntInclusive(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 document.body.addEventListener('submit', async (e) => {
@@ -29,8 +33,26 @@ document.body.addEventListener('submit', async (e) => {
   })
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
-      // You're going to do your lab work in here. Replace this comment.
-      console.log('fromServer', fromServer);
+      if (document.querySelector('.flex-inner')) {
+        document.querySelector('.flex-inner').remove();
+      }
+      const newArr = range(10);
+      const newArr2 = newArr.map(() => {
+        const number = getRandomIntInclusive(243);
+        return fromServer[number];
+      });
+
+      const reverseList = newArr2.sort((a, b) => sortByKey(a, b, 'name'));
+      const ol = document.createElement('ol');
+      ol.className = 'flex-inner';
+      $('form').prepend(ol);
+
+      reverseList.forEach((el) => {
+        const li = document.createElement('li');
+        $(li).append(`<input type="checkbox" values=${el.code} id=${el.code} />`);
+        $(li).append(`<label for=${el.code}>${el.name}</label>`);
+        (ol).appendChild(li);
+      });
     })
     .catch((err) => console.log(err));
 });
